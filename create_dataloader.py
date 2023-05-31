78f6bdb58a8bb10e2ec1d9820edd9bc67752fada
Korneel Van den Berghe <k.c.m.vandenberghe@student.tudelft.nl>
2023-05-31 13:49:15 +0200

    Fix Dataloader

--- create_dataloader.py
+++ create_dataloader.py
@@ -16,61 +16,71 @@ import matplotlib.pyplot as plt
 # print(df.head())
 # print(bounding_frame.info())
 
-class KenyaDataset(Dataset):
-    '''Dataset with wildlife images from the iWildLife competition.
-    '''
+# class KenyaDataset(Dataset):
+#     '''Dataset with wildlife images from the iWildLife competition.
+#     '''
 
-    def __init__(self, csv_file, root_dir, transform = None) -> None:
-        super(Dataset, self).__init__()
-        """
-        Arguments:
-            csv_file (string): Path to the csv file with annotations.
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
-        """
-        self.labels = pd.read_csv(csv_file)
-        self.root_dir = root_dir
-        self.transform = transform
+#     def __init__(self, csv_file, root_dir, transform = None) -> None:
+#         super(Dataset, self).__init__()
+#         """
+#         Arguments:
+#             csv_file (string): Path to the csv file with annotations.
+#             root_dir (string): Directory with all the images.
+#             transform (callable, optional): Optional transform to be applied
+#                 on a sample.
+#         """
+#         self.labels = pd.read_csv(csv_file)
+#         self.root_dir = root_dir
+#         self.transform = transform
 
-    def __len__(self):
-        return len(self.labels)
+#     def __len__(self):
+#         return len(self.labels)
     
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+#     def __getitem__(self, idx):
+#         if torch.is_tensor(idx):
+#             idx = idx.tolist()
 
-        print(self.labels.iloc[idx, 1])
-        img_name = os.path.join(self.root_dir,
-                                self.labels.iloc[idx, 1])
+#         print(self.labels.iloc[idx, 1])
+#         img_name = os.path.join(self.root_dir,
+#                                 self.labels.iloc[idx, 1])
         
-        image = Image.open(img_name)
-        landmarks = self.labels.iloc[idx, 2:]
-        # landmarks = np.array([landmarks])
-        # landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
+#         image = Image.open(img_name)
+#         landmarks = self.labels.iloc[idx, 2:]
+#         # landmarks = np.array([landmarks])
+#         # landmarks = landmarks.astype('float').reshape(-1, 2)
+#         sample = {'image': image, 'landmarks': landmarks}
 
-        if self.transform:
-            sample = self.transform(sample)
+#         if self.transform:
+#             sample = self.transform(sample)
 
-        return sample
+#         return sample
     
 
 
-kenya_dataset = KenyaDataset('kenya_labels.csv', 'kenya_ims')
-print(len(kenya_dataset))
-sample = kenya_dataset[2]['image']
-plt.imshow(sample)
-plt.show()
+# kenya_dataset = KenyaDataset('kenya_labels.csv', 'kenya_ims')
+# print(len(kenya_dataset))
+# sample = kenya_dataset[2]['image']
+# plt.imshow(sample)
+# plt.show()
 
-transformed_dataset = KenyaDataset(csv_file='kenya_labels.csv',
-                                           root_dir='kenya_ims',
-                                           transform=None
-                                           )
-dataloader = DataLoader(transformed_dataset, batch_size=4,
-                        shuffle=True, num_workers=0)from torch.utils.data import Dataset, DataLoader
+# transformed_dataset = KenyaDataset(csv_file='kenya_labels.csv',
+#                                            root_dir='kenya_ims',
+#                                            transform=None
+#                                            )
+# dataloader = DataLoader(transformed_dataset, batch_size=4,
+#                         shuffle=True, num_workers=0)
+from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import torch
+from torchvision import transforms
+
+transform = transforms.Compose([
+    # you can add other transformations in this list
+    transforms.Grayscale(),
+    transforms.Resize((240,240)),
+    transforms.ToTensor()
+    
+])
 
 import json
 import pandas as pd
@@ -80,12 +90,6 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-# data = json.load(open('iwildcam2022_mdv4_detections.json'))
-# bounding_frame = pd.DataFrame(data['images'])
-
-# print(df.head())
-# print(bounding_frame.info())
-
 class KenyaDataset(Dataset):
     '''Dataset with wildlife images from the iWildLife competition.
     '''
@@ -100,6 +104,7 @@ class KenyaDataset(Dataset):
                 on a sample.
         """
         self.labels = pd.read_csv(csv_file)
+        # print(self.labels.info())
         self.root_dir = root_dir
         self.transform = transform
 
@@ -115,27 +120,32 @@ class KenyaDataset(Dataset):
                                 self.labels.iloc[idx, 1])
         
         image = Image.open(img_name)
-        landmarks = self.labels.iloc[idx, 2:]
+        landmarks = self.labels.iloc[idx, 2:].to_dict()
         # landmarks = np.array([landmarks])
         # landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
+        
 
         if self.transform:
-            sample = self.transform(sample)
+            # print(sample['image'])
+            image = self.transform(image)
 
+        sample = {'image': image, 'landmarks': landmarks}
         return sample
     
 
 
-kenya_dataset = KenyaDataset('kenya_labels.csv', 'kenya_ims')
-print(len(kenya_dataset))
-sample = kenya_dataset[2]['image']
-plt.imshow(sample)
-plt.show()
+kenya_dataset = KenyaDataset('kenya_labels.csv', 'kenya_ims', transform=transform)
+# print(len(kenya_dataset))
+sample = kenya_dataset[3]['image']
+# print(kenya_dataset[3]['landmarks'].value())
+# plt.imshow(sample)
+# plt.show()
 
 transformed_dataset = KenyaDataset(csv_file='kenya_labels.csv',
                                            root_dir='kenya_ims',
-                                           transform=None
+                                           transform=transform
                                            )
+
 dataloader = DataLoader(transformed_dataset, batch_size=4,
-                        shuffle=True, num_workers=0)
+                        shuffle=True)
+print(next(iter(dataloader)))
