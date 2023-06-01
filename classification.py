cbdaefb5272b392de17c291ddcbbe0ba41b3d4f3
sebastienvandenabeele <svandenabeele@student.tudelft.nl>
2023-06-01 21:53:50 +0200

    trouble shooting classification

--- classification.py
+++ classification.py
@@ -26,29 +26,32 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
-import numpy as np
 
 import torchvision
 import torchvision.transforms as transforms
-#from torchvision.models.mobilenetv3 import MobileNet_V3_Small_Weights
 
-from tqdm import tqdm # progress bar
+# from torchvision.models.mobilenetv3 import MobileNet_V3_Small_Weights
+
+from tqdm import tqdm  # progress bar
 
 # Import from project
-from create_dataloader import dataloader
-#from optimizing import create_optimizer, BoundingBoxLoss, CrossEntropyLoss
+from create_data_loader import dataloader
+
+# from optimizing import create_optimizer, BoundingBoxLoss, CrossEntropyLoss
 
 ######### Definitions
-classes = 2 # no humans nor vehicles in the dataset
+classes = 2  # no humans nor vehicles in the dataset
 epochs = 10
 #########
 
 ######### Create the model
 # Load the model
-model = torchvision.models.mobilenet_v3_small(pretrained=True) #pretrained will become deprecated in the future
+model = torchvision.models.mobilenet_v3_small(
+    pretrained=True
+)  # pretrained will become deprecated in the future
 # modify the last layer
-model.classifier[3] = torch.nn.Linear(in_features=1024, out_features= classes, bias=True)
-# freeze all layers 
+model.classifier[3] = torch.nn.Linear(in_features=1024, out_features=classes, bias=True)
+# freeze all layers
 for param in model.parameters():
     param.requires_grad = False
 # unfreeze the last layer
@@ -58,73 +61,100 @@ for param in model.classifier[3].parameters():
 
 # loss function
 criterion = nn.CrossEntropyLoss()
-#optimizer
+# optimizer
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# Create dataloader from the training and test set 
+# Create dataloader from the training and test set
 train_loader = dataloader
 test_loader = []
 
-#loss = criterion(torch.tensor([1.0]), torch.tensor([1.0]))
-#print('loss58', loss)
+loss = criterion(torch.tensor([1.0]), torch.tensor([1.0]))
+print('loss58', loss)
 
-######### Training 
+######### Training
 
-#print(model)
+
+# Y_pred_good = torch.tensor[2.1, 1.0, 2.1]
+# print(model)
+# labels = torch.tensor([1,0,1,0])
+# Y = torch.tensor([2,0,1])
+
+
+# Y = torch.tensor([2,0,1,1])
+# print('Y', Y)
+# print('Y.size', Y.size())
+# Y_pred_good = torch.tensor([[0.1, 1.0, 2.1], [0.1, 1.0, 2.1], [0.1, 1.0, 2.1], [0.1, 1.0, 2.1]])
+# #Y_pred_good = torch.tensor([[2.1, 1.0, 2.1], [1.1, 1.0, 2.1], [2.1, 1.0, 2.1]])
+# #[0.1, 1.0, 2.1]]
+# print(Y_pred_good)
+# print('shape', Y_pred_good.size())
+
+# l1 = criterion(Y_pred_good, Y)
+
+# print('l1', l1)
+
 
 def train(model, criterion, optimizer, train_loader):
-    """ 
+    """
     Trains network for one epoch in batches.
     Args:
         train_loader: Data loader for training set.
-        model: MobileNetV3 
+        model: MobileNetV3
         optimizer: Adam
         criterion: CrossEntropyLoss
     """
     avg_loss = 0
     correct = 0
     total = 0
-    
-    #for batch, data in enumerate(train_loader):
-    #print(train_loader)
+
+    # for batch, data in enumerate(train_loader):
+    # print(train_loader)
     model.train()
     for data in train_loader:
-        #inputs, labels = data
-        inputs = data['image']
-        labels = data['landmarks']['max_detection_conf']
+        # inputs, labels = data
+        inputs = data["image"]
+        labels = data["landmarks"]["max_detection_conf"]
 
-        if labels == ['0.0']: 
-            labels = torch.tensor([[1, 0],[1, 0] ,[1, 0] ,[1, 0]]) # target of cross-enropy loss should be class index
+        print("labels", labels)
+
+        if labels == ["0.0"]:
+            # labels = torch.tensor([[1, 0],[1, 0] ,[1, 0] ,[1, 0]]) # target of cross-enropy loss should be class index
+            labels = torch.tensor([1, 0])
         else:
-            labels = torch.tensor([[1, 0],[1, 0] ,[1, 0] ,[1, 0]]) 
-            
+            # labels = torch.tensor([[1, 0],[1, 0] ,[1, 0] ,[1, 0]])
+            labels = torch.tensor([1, 0, 1, 0])
+
             # labels is random tensor with batch size 4
 
-        print('shape', labels.size())
+        print("labels", labels)
+
+        print("shape", labels.size())
         # unsqueeze the labels
-        labels1 = labels #.unsqueeze(0)
-        print('labels' , labels1)
+        labels1 = labels  # .unsqueeze(0)
+        print("labels", labels1)
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # forward + backward + optimize
         outputs = model(inputs)
-        print('outputs' , outputs)
 
-        loss = criterion(inputs, labels1)
+        print("outputs", outputs)
+        print(outputs.size())
+
+        loss = criterion(outputs, labels1)
         loss.backward()
         optimizer.step()
 
-        
         avg_loss += loss
         _, predicted = torch.max(outputs.data, 1)
-        #total += labels.size(0)
+        # total += labels.size(0)
         total = 2
-        #correct += (predicted == labels).sum().item()
+        # correct += (predicted == labels).sum().item()
         correct = 1
 
-    return avg_loss/len(train_loader), 100 * correct / total
-        
+    return avg_loss / len(train_loader), 100 * correct / total
+
+
 ######### Testing
 def test(test_loader, model, criterion):
     """
@@ -137,30 +167,31 @@ def test(test_loader, model, criterion):
     avg_loss = 0
     correct = 0
     total = 0
-    
+
     model.eval()
     # Use torch.no_grad to skip gradient calculation, not needed for evaluation
     with torch.no_grad():
         # iterate through batches
         for data in test_loader:
             # get the inputs; data is a list of [inputs, labels]
-            
-            inputs = data['image']
-            labels = data['landmarks']['max_detection_conf']
+
+            inputs = data["image"]
+            labels = data["landmarks"]["max_detection_conf"]
 
             # forward pass
             outputs = model(inputs)
-           
+
             loss = criterion(outputs, labels)
-            labels2 = np.array([1,2])
+            labels2 = np.array([1, 2])
             # keep track of loss and accuracy
             avg_loss += loss
             _, predicted = torch.max(outputs.data, 1)
             total += labels2.size(0)
-            
+
             correct += (predicted == labels).sum().item()
 
-    return avg_loss/len(test_loader), 100 * correct / total
+    return avg_loss / len(test_loader), 100 * correct / total
+
 
 print(train_loader)
 
@@ -168,7 +199,13 @@ print(train_loader)
 for epoch in tqdm(range(epochs)):  # loop over the dataset multiple times
     # Train on data
     train_loss, train_acc = train(model, criterion, optimizer, train_loader)
+
     # Test on data
-    #test_loss, test_acc = test(test_loader, model, criterion)
+    # test_loss, test_acc = test(test_loader, model, criterion)
+
+    # Print training results
+    print(
+        "Epoch: {}, Train Loss: {}, Train Acc: {}".format(epoch, train_loss, train_acc)
+    )
     # Print results
-    print('Epoch: {}, Train Loss: {}, Train Acc: {}, Test Loss: {}, Test Acc: {}'.format(epoch, train_loss, train_acc, test_loss, test_acc))
+    # print('Epoch: {}, Train Loss: {}, Train Acc: {}, Test Loss: {}, Test Acc: {}'.format(epoch, train_loss, train_acc, test_loss, test_acc))
--- create_data_loader.py
+++ create_data_loader.py
@@ -5,22 +5,23 @@ import torch
 import json
 import pandas as pd
 import os
+
 # from skimage import io, transform
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-# data = json.load(open('iwildcam2022_mdv4_detections.json'))
-# bounding_frame = pd.DataFrame(data['images'])
+# data = json.load(open("iwildcam2022_mdv4_detections.json"))
+# bounding_frame = pd.DataFrame(data["images"])
 
 # print(df.head())
 # print(bounding_frame.info())
 
-class KenyaDataset(Dataset):
-    '''Dataset with wildlife images from the iWildLife competition.
-    '''
 
-    def __init__(self, csv_file, root_dir, transform = None) -> None:
+class KenyaDataset(Dataset):
+    """Dataset with wildlife images from the iWildLife competition."""
+
+    def __init__(self, csv_file, root_dir, transform=None) -> None:
         super(Dataset, self).__init__()
         """
         Arguments:
@@ -35,36 +36,35 @@ class KenyaDataset(Dataset):
 
     def __len__(self):
         return len(self.labels)
-    
+
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
         print(self.labels.iloc[idx, 1])
-        img_name = os.path.join(self.root_dir,
-                                self.labels.iloc[idx, 1])
-        
+        img_name = os.path.join(self.root_dir, self.labels.iloc[idx, 1])
+
         image = Image.open(img_name)
         landmarks = self.labels.iloc[idx, 2:]
         # landmarks = np.array([landmarks])
         # landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
+        sample = {"image": image, "landmarks": landmarks}
 
         if self.transform:
             sample = self.transform(sample)
 
         return sample
-    
 
-kenya_dataset = KenyaDataset('kenya_labels.csv', 'kenya_ims')
-print(len(kenya_dataset))
-sample = kenya_dataset[2]['image']
-plt.imshow(sample)
-plt.show()
 
-transformed_dataset = KenyaDataset(csv_file='kenya_labels.csv',
-                                           root_dir='kenya_ims',
-                                           transform=None
-                                           )
-dataloader = DataLoader(transformed_dataset, batch_size=4,
-                        shuffle=True, num_workers=0)
+kenya_dataset = KenyaDataset("kenya_labels.csv", "./kenya_ims")
+
+# print test
+# print(len(kenya_dataset))
+# sample = kenya_dataset[2]["image"]
+# plt.imshow(sample)
+# plt.show()
+
+transformed_dataset = KenyaDataset(
+    csv_file="kenya_labels.csv", root_dir="./kenya_ims", transform=None
+)
+dataloader = DataLoader(transformed_dataset, batch_size=4, shuffle=True, num_workers=0)
